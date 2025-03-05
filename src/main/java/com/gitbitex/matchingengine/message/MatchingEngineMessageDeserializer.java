a26d875fdbb4fdb74e6d51cb119442a4d6fa4cbe
dinhd <dinh.duke@gmail.com>
2025-03-05 08:58:48 -0600

    Replaced Charset.defaultCharset() with StandardCharsets.UTF_8

--- src/main/java/com/gitbitex/matchingengine/message/MatchingEngineMessageDeserializer.java
+++ src/main/java/com/gitbitex/matchingengine/message/MatchingEngineMessageDeserializer.java
@@ -4,7 +4,7 @@ import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Deserializer;
 
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 public class MatchingEngineMessageDeserializer implements Deserializer<Message> {
@@ -14,31 +14,23 @@ public class MatchingEngineMessageDeserializer implements Deserializer<Message> 
             MessageType messageType = MessageType.valueOfByte(bytes[0]);
             switch (messageType) {
                 case COMMAND_START:
-                    return JSON.parseObject(bytes, 1, bytes.length - 1, Charset.defaultCharset(),
-                            CommandStartMessage.class);
+                    return JSON.parseObject(bytes, 1, bytes.length - 1, StandardCharsets.UTF_8, CommandStartMessage.class);
                 case COMMAND_END:
-                    return JSON.parseObject(bytes, 1, bytes.length - 1, Charset.defaultCharset(),
-                            CommandEndMessage.class);
+                    return JSON.parseObject(bytes, 1, bytes.length - 1, StandardCharsets.UTF_8, CommandEndMessage.class);
                 case ACCOUNT:
-                    return JSON.parseObject(bytes, 1, bytes.length - 1, Charset.defaultCharset(),
-                            AccountMessage.class);
+                    return JSON.parseObject(bytes, 1, bytes.length - 1, StandardCharsets.UTF_8, AccountMessage.class);
                 case PRODUCT:
-                    return JSON.parseObject(bytes, 1, bytes.length - 1, Charset.defaultCharset(),
-                            ProductMessage.class);
+                    return JSON.parseObject(bytes, 1, bytes.length - 1, StandardCharsets.UTF_8, ProductMessage.class);
                 case ORDER:
-                    return JSON.parseObject(bytes, 1, bytes.length - 1, Charset.defaultCharset(),
-                            OrderMessage.class);
+                    return JSON.parseObject(bytes, 1, bytes.length - 1, StandardCharsets.UTF_8, OrderMessage.class);
                 case TRADE:
-                    return JSON.parseObject(bytes, 1, bytes.length - 1, Charset.defaultCharset(),
-                            TradeMessage.class);
+                    return JSON.parseObject(bytes, 1, bytes.length - 1, StandardCharsets.UTF_8, TradeMessage.class);
                 default:
                     logger.warn("Unhandled order message type: {}", messageType);
-                    return JSON.parseObject(bytes, 1, bytes.length - 1, Charset.defaultCharset(),
-                            Message.class);
+                    return JSON.parseObject(bytes, 1, bytes.length - 1, StandardCharsets.UTF_8, Message.class);
             }
         } catch (Exception e) {
-            throw new RuntimeException("deserialize error: " + new String(bytes), e);
+            throw new RuntimeException("deserialize error: " + new String(bytes, StandardCharsets.UTF_8), e);
         }
     }
 }
-

36861ef0c20047ca5db375b4521d236ddf4d9c40
dinhd <dinh.duke@gmail.com>
2025-03-05 08:53:27 -0600

    explicitly setting UTF-8

--- src/main/java/com/gitbitex/matchingengine/command/CommandDeserializer.java
+++ src/main/java/com/gitbitex/matchingengine/command/CommandDeserializer.java
@@ -3,8 +3,7 @@ package com.gitbitex.matchingengine.command;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Deserializer;
-
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 public class CommandDeserializer implements Deserializer<Command> {
@@ -14,17 +13,16 @@ public class CommandDeserializer implements Deserializer<Command> {
             CommandType commandType = CommandType.valueOfByte(bytes[0]);
             return switch (commandType) {
                 case PUT_PRODUCT ->
-                        JSON.parseObject(bytes, 1, bytes.length - 1, Charset.defaultCharset(), PutProductCommand.class);
+                        JSON.parseObject(bytes, 1, bytes.length - 1, StandardCharsets.UTF_8, PutProductCommand.class);
                 case DEPOSIT ->
-                        JSON.parseObject(bytes, 1, bytes.length - 1, Charset.defaultCharset(), DepositCommand.class);
-                case PLACE_ORDER -> JSON.parseObject(bytes, 1, bytes.length - 1, Charset.defaultCharset(),
+                        JSON.parseObject(bytes, 1, bytes.length - 1, StandardCharsets.UTF_8, DepositCommand.class);
+                case PLACE_ORDER -> JSON.parseObject(bytes, 1, bytes.length - 1, StandardCharsets.UTF_8,
                         PlaceOrderCommand.class);
-                case CANCEL_ORDER -> JSON.parseObject(bytes, 1, bytes.length - 1, Charset.defaultCharset(),
+                case CANCEL_ORDER -> JSON.parseObject(bytes, 1, bytes.length - 1, StandardCharsets.UTF_8,
                         CancelOrderCommand.class);
                 default -> {
                     logger.warn("Unhandled order message type: {}", commandType);
-                    yield JSON.parseObject(bytes, 1, bytes.length - 1, Charset.defaultCharset(),
-                            Command.class);
+                    yield JSON.parseObject(bytes, 1, bytes.length - 1, StandardCharsets.UTF_8, Command.class);
                 }
             };
         } catch (Exception e) {
